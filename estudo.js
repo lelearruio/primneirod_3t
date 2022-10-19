72a625b1d8e17169469e6bcb71ca246b981d148e
lelearruio <108942472+lelearruio@users.noreply.github.com>
2022-10-19 11:10:25 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,13 +1,26 @@
 function total(){
-  let c = document.getElementById("valor").value;
+    let c = document.getElementById("valor").value;
     let j = document.getElementById("juros").value;
-      let t = document.getElementById("meses").value;
-      let r = 0;
+    let t = document.getElementById("meses").value;
+    let r = 0;
     for(let i = 1; i <= t; i++){
         r = c * (1 + (j/100));
-        document.write("Mês " + i + " valor: " + r +"<br>")
+        document.write("Mês " + i + " valor: " + r +"<br>");
         c = r;
     }
-
+    
     document.write("Resultado: " + r);
+}
+function adicao () {
+     let val1 = document.getElementById("v1").value;
+     let val2 = document.getElementById("v2").value;
+     let r = Number( val1 ) + Number( val2 );
+     document.getElementById(" resultado ").innerHTML = r;
+}
+
+function subtracao () {
+    let val1 = document.getElementById("v1").value;
+    let val2 = document.getElementById("v2").value;
+    let r = Number( val1 ) - Number( val2 );
+    document.getElementById(" resultado ").innerHTML = r;
 }
